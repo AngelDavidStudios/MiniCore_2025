f14d6371797b129d098ca66b54a2a1eaba464a60
Angel David <angeldavidstudios@outlook.com>
2025-07-13 20:35:56 -0500

    Empaquetacion lambda mediante serverless framework

---
+++ Comision.API/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#install zip on debian OS, since microsoft/dotnet container doesn't have zip by default
+if [ -f /etc/debian_version ]
+then
+  apt -qq update
+  apt -qq -y install zip
+fi
+
+dotnet restore
+dotnet lambda package --configuration Debug --framework net8.0 --output-package bin/release/net8.0/comisionApi.zip
